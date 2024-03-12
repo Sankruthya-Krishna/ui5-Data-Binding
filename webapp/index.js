43c2cdfc0bdd510fd2bd1c1e0b9714a471a97f1c
Sankruthya-Krishna <sankruthyakrishna13@gmail.com>
2024-03-12 14:28:05 +0530

    step-4(two way binding)

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,5 @@
 sap.ui.require([
-	"sap/m/Text",
+	"sap/ui/core/mvc/XMLView",
     "sap/ui/model/json/JSONModel"
 ], function (Text,JSONModel) {
 	"use strict";
@@ -8,13 +8,16 @@ sap.ui.require([
 	sap.ui.getCore().attachInit(function () {
         // Create a JSON model from an object literal
 		var oModel = new JSONModel({
-			greetingText: "Hi, my name is Harry Hawk"
+			firstName: "Harry",
+			lastName: "Hawk",
+			enabled: true,
+			panelHeaderText: "Data Binding Basics"
 		});
         // Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
-		// Display a text element whose text is derived
-		// from the model object
-
-		new Text({text: "{/greetingText}"}).placeAt("content");
+		// Display the XML view called "App"
+		new XMLView({
+			viewName: "sap.ui.demo.db.view.App"
+		}).placeAt("content");
 	});
 });
